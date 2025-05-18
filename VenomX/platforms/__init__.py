911054a0f7b49d90d27ed6e9d6f213c11389d29c
VT7org <glitchnaxusofficial@gmail.com>
2025-05-18 20:54:41 +0530

    Update __init__.py

--- VenomX/platforms/__init__.py
+++ VenomX/platforms/__init__.py
@@ -1,7 +1,3 @@
-
-# All rights reserved.
-#
-
 from .Apple import Apple
 from .Carbon import Carbon
 from .JioSavan import Saavn
@@ -10,15 +6,3 @@ from .Soundcloud import SoundCloud
 from .Spotify import Spotify
 from .Telegram import Telegram
 from .Youtube import YouTube
-
-
-class PlaTForms:
-    def __init__(self):
-        self.apple = Apple()
-        self.carbon = Carbon()
-        self.saavn = Saavn()
-        self.resso = Resso()
-        self.soundcloud = SoundCloud()
-        self.spotify = Spotify()
-        self.telegram = Telegram()
-        self.youtube = YouTube()
