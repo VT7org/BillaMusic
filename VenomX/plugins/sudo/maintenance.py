ecd79db66953fbd3c5121fc9ade38203b7f4deb7
VT7org <glitchnaxusofficial@gmail.com>
2025-05-18 22:16:22 +0530

    Update maintenance.py

--- VenomX/plugins/sudo/maintenance.py
+++ VenomX/plugins/sudo/maintenance.py
@@ -15,29 +15,27 @@ from VenomX.utils.database import (
 )
 
 
-@app.on_message(command("MAINTENANCE_COMMAND") & SUDOERS)
+@app.on_message(filters.command(["maintenance"]) & SUDOERS)
 async def maintenance(client, message: Message):
     try:
         language = await get_lang(message.chat.id)
         _ = get_string(language)
-    except Exception:
+    except:
         _ = get_string("en")
     usage = _["maint_1"]
     if len(message.command) != 2:
         return await message.reply_text(usage)
-    message.chat.id
-    state = message.text.split(None, 1)[1].strip()
-    state = state.lower()
+    state = message.text.split(None, 1)[1].strip().lower()
     if state == "enable":
         if await is_maintenance() is False:
             await message.reply_text(_["maint_6"])
         else:
             await maintenance_on()
-            await message.reply_text(_["maint_2"])
+            await message.reply_text(_["maint_2"].format(app.mention))
     elif state == "disable":
         if await is_maintenance() is False:
             await maintenance_off()
-            await message.reply_text(_["maint_3"])
+            await message.reply_text(_["maint_3"].format(app.mention))
         else:
             await message.reply_text(_["maint_5"])
     else:
