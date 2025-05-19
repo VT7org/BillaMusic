e7086b99a2741e4bbfb50a05e1802c1728d377ef
VT7org <glitchnaxusofficial@gmail.com>
2025-05-19 11:05:34 +0530

    Update queue.py

--- VenomX/utils/stream/queue.py
+++ VenomX/utils/stream/queue.py
@@ -45,7 +45,7 @@ async def put_queue(
     else:
         db[chat_id].append(put)
     autoclean.append(file)
-    vidid = "telegram" if vidid == "soundcloud" or "saavn" in vidid else vidid
+    vidid = "Telegram" if vidid == "Soundcloud" or "Saavn" in vidid else vidid
 
     to_append = {"vidid": vidid, "title": title}
     if chat_id not in chatstats:
